18ea5baef138c973edb5a47b1b0521440057162e
onghdzaa <gamezzbbzz@gmail.com>
2024-02-27 12:36:48 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ThailandAccessibleTourism/'
-    : '/'
+  pwa: {
+      workboxOptions: {
+        exclude: [/ThailandAccessibleTourism/]
+      }
+    }
 }
