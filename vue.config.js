0df6196da395fe117d01f2414890566c38921ad1
onghdzaa <gamezzbbzz@gmail.com>
2024-02-27 07:53:38 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ThailandAccessibleTourism/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/ThailandAccessibleTourism/'
+//     : '/'
+// }
